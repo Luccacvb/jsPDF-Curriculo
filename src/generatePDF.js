1dd0f234633ce5ab1dee4e05859aeac91d823787
lucca <lucca.boas@kartado.com.br>
2023-08-13 14:02:17 -0300

    Add draw color in pdf

--- src/generatePDF.js
+++ src/generatePDF.js
@@ -3,14 +3,21 @@ import { jsPDF } from 'jspdf'
 function generatePDF() {
 	const doc = new jsPDF()
 
+	// Defina a cor do traço (borda) para azul
+	doc.setDrawColor(0, 0, 255)
+
+	// Desenha um retângulo no tamanho total da página
+	doc.rect(10, 10, 190, 277, 'S')
+
+	//Adiciona textos
 	doc.setFontSize(24)
-	doc.text('Seu Nome', 10, 10)
+	doc.text('Lucca Carvalho Vilas Boas', 20, 30)
 
 	doc.setFontSize(16)
-	doc.text('Sua Profissão', 10, 30)
+	doc.text('Sua Profissão', 20, 50)
 
 	doc.setFontSize(12)
-	doc.text('Detalhes e experiência...', 10, 50)
+	doc.text('Detalhes e experiência...', 20, 70)
 
 	doc.save('Curriculo_Seunome.pdf')
 }
