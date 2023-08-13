0302d4853d870713acb351f68174ebb5610f9618
lucca <lucca.boas@kartado.com.br>
2023-08-13 13:03:13 -0300

    First commit test jsPDF study

---
+++ src/generatePDF.js
@@ -0,0 +1,18 @@
+import { jsPDF } from 'jspdf';
+
+function generatePDF() {
+    const doc = new jsPDF();
+
+    doc.setFontSize(24);
+    doc.text("Seu Nome", 10, 10);
+
+    doc.setFontSize(16);
+    doc.text("Sua Profissão", 10, 30);
+
+    doc.setFontSize(12);
+    doc.text("Detalhes e experiência...", 10, 50);
+
+    doc.save("Curriculo_Seunome.pdf");
+}
+
+export default generatePDF;
