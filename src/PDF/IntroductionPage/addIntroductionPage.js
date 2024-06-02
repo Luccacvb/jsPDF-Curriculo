7d55a530098bd22d56b3b63e2ee96cb4e1e59722
lucca <lucca.boas@kartado.com.br>
2024-06-02 14:27:32 -0300

    feat: update resume

--- src/PDF/IntroductionPage/addIntroductionPage.js
+++ src/PDF/IntroductionPage/addIntroductionPage.js
@@ -10,7 +10,7 @@ export function addIntroductionPage(doc) {
 		doc,
 		25,
 		'Objetivo Profissional',
-		'Desenvolvedor Front-end e Mobile com experiência em JavaScript, TypeScript, React e React Native, em busca de oportunidades para evolução técnica e melhorias financeiras. Motivação por desafios que promovam o crescimento em desenvolvimento de software e inteligência artificial. Interessado em contribuir para projetos inovadores e colaborativos. Entre os objetivos pessoais estão viajar mais, conhecer novas culturas e evoluir no esporte que pratico, paraquedismo',
+		'Desenvolvedor Front-end e Mobile com experiência em JavaScript, TypeScript, React e React Native, buscando oportunidades para evolução técnica e novos conceitos. Motivado por desafios que promovam crescimento no desenvolvimento de software e inteligência artificial. Interessado em contribuir para projetos inovadores e colaborativos. Almejo fazer uma contribuição significativa em projetos que impactem positivamente a sociedade.',
 	)
 
 	// Seção - Habilidades Técnicas
@@ -18,35 +18,38 @@ export function addIntroductionPage(doc) {
 		doc,
 		65,
 		'Habilidades Técnicas',
-		'Linguagens de Programação: JavaScript, TypeScript, Python\nFrameworks e Bibliotecas: React, React Native, Redux, Redux-Saga, WatermelonDB, TensorFlow.js\nFerramentas de Desenvolvimento: Git, Docker, Django REST Framework\nConceitos de IA: Fundamentos de machine learning e integração com APIs de inteligência artificial',
+		'Linguagens de Programação: JavaScript, TypeScript, Python\nFrameworks e Bibliotecas: React, React Native, Redux, Redux-Saga, WatermelonDB, TensorFlow.js\nFerramentas de Desenvolvimento: Git, Docker, Django REST Framework\nConceitos de IA: Fundamentos de aprendizado de máquina e integração com APIs de inteligência artificial',
 	)
 
 	// Seção - Experiência Profissional
 	addSection(
 		doc,
-		120,
+		115,
 		'Experiência Profissional',
-		'Desenvolvedor de Software (Front-end e Mobile) na Kartado, desde 2022 até o momento atual\nAtuação com desenvolvimento de soluções front-end e mobile\nRevisões de código\nEspecificações técnicas\nResolução de problemas em parceria com a equipe de suporte\nProatividade na aprendizagem e aplicação de conceitos avançados como de manipulação de dados em larga escala e utilização de ferramentas de colaboração como o Git',
+		'Desenvolvedor de Software (Front-end e Mobile) na Kartado, de 2022 até o presente\nEngajamento no desenvolvimento de soluções front-end e mobile\nRevisões de código\nEspecificações técnicas\nResolução de problemas em colaboração com a equipe de suporte\nProativo na aprendizagem e aplicação de conceitos avançados como manipulação de dados em larga escala e uso de ferramentas de colaboração como Git',
 	)
 
 	// Seção - Projetos
 	addSection(
 		doc,
-		187,
-		'Projeto Atual de Estudo',
-		'Integração do ChatGPT com Venom Bot: Projeto pessoal voltado ao desenvolvimento de uma inteligência artificial para automatizar respostas em mensagens pelo whatsApp, utilizando Node.js e integrando a API do ChatGPT',
+		182,
+		'Projeto de Estudo Atual',
+		'Integração do ChatGPT com Venom Bot: Um projeto pessoal visando desenvolver inteligência artificial para automatizar respostas em mensagens via WhatsApp, usando Node.js e integrando a API do ChatGPT',
 	)
 
-	// Seção - Educação
+	// Seção - Formação
 	addSection(
 		doc,
-		220,
-		'Educação',
-		'Curso de Programação (JavaScript e Python), Jcavi Treinamentos, 2021 a 2022\nGestão da Tecnologia, Unisul, iniciado em 2022 (atualmente esta trancada, mas pretendo finalizar)',
+		210,
+		'Formação',
+		'Curso de Programação (JavaScript e Python), Jcavi Treinamentos, 2021 a 2022\nGestão da Tecnologia, Unisul, iniciado em 2022 (atualmente trancado, mas com intenção de concluir)',
 	)
 
 	// Seção - Interesses Pessoais
-	addSection(doc, 255, 'Interesses Pessoais', 'Paraquedismo, surf, viagens, exploração cultural')
+	addSection(doc, 245, 'Interesses Pessoais', 'Paraquedismo, surfe, viagens, exploração cultural')
 
-	doc.save('Curriculo.pdf')
+	//Idiomas
+	addSection(doc, 269, 'Idiomas', 'Português: Nativo, Inglês: Básico, Espanhol: Intermediário')
+
+	doc.save('Currículo.pdf')
 }
